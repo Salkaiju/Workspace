c344272755cf6f42d2e60b6d52843d8a4ff40b58
Salkaiju <gabrielkarkotli@live.comm>
2025-07-09 11:26:49 -0300

    2

--- Desafios/script.js
+++ Desafios/script.js
@@ -67,4 +67,4 @@ function desafios() {
                     alert("Idade menor que 18.");
                 }
             
-        }
+        };
---
+++ Desafios2/script.js
@@ -0,0 +1,65 @@
+function desafio(){
+    let dia;
+    dia = prompt("Digite o dia");
+    switch(dia){
+        case "Sábado":{
+            alert("Bom fim de semana!");
+        } break;
+        case "Domingo":{
+            alert("Bom fim de semana!")
+        } break;
+        default:{
+            alert("Boa semana!");
+        }
+    }
+}
+
+function desafio2(){
+    let n;
+    do {
+    n = prompt("Digite um número aí vai, bem bomitimh vai");
+    if (isNaN(n)){
+        alert("Erro. Digite um número válido!");
+    }
+    } while(isNaN(n));
+    if (n == 0){
+        alert("Você digitou 0, ou nulo.");
+    } else if (n >= 0){
+        alert("Positivo");
+    } else if (n <= 0){
+        alert("Negativo");
+    }
+}
+
+function desafio3(){
+    let n;
+    do {
+    n = prompt("Digite um número maior que 100 ai vai");
+    if (isNaN(n)){
+        alert("Erro. Digite um número válido!");
+    }
+    } while(isNaN(n));
+    if (n == 100){
+        alert("Você pontoou 100!");
+    }
+    else if (n > 100){
+        alert("Você pontoou" + n + "! e É maior que 100!!! para bennss!!");
+    } else if (n < 100){
+        alert("Você pontoou " + n + ", Mas é menor que 100, logo, você perdeu seu perdedor.");
+    }
+}
+
+function desafio4(){
+    let saldo;
+    saldo = prompt("Digite seu saldo ai vai.");
+    alert("Seu saldo é " + saldo + ", mermão.")
+}
+
+function desafio5(){
+    let nome;
+    nome = prompt("Digite o seu nome aiii");
+    alert("Belezinha, " + nome + ", se escreveu!!!");
+}
+
+
+
