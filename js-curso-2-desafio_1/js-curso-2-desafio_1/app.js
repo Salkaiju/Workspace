bad72b3229dea208943437d642b72b2a43338031
Salkaiju <gabrielkarkotli@live.comm>
2025-07-14 11:32:33 -0300

    3

---
+++ js-curso-2-desafio_1/js-curso-2-desafio_1/app.js
@@ -0,0 +1,25 @@
+let h1 = document.querySelector("h1");
+h1.innerHTML = "Hora do desafio."
+
+function cc(){
+    console.log("O botão foi clicado");
+}
+
+function ccOdeio(){
+    alert("eu odeio o JS");
+}
+
+function perguntaCidade(){
+    let cidade = prompt("Diga o nome de uma cidade do brasil.");
+    alert("Já fui em " + cidade + " e ODIEI.");
+}
+
+function soma(){
+    let n1 = Number(prompt("Digite um número."));
+    let n2 = Number(prompt("Digite outro número"));
+    let n3 = n1 + n2;
+    alert(n3);
+}
+
+
+
--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -2,7 +2,7 @@ const funAle = (min, max) => {
     min = Math.ceil(min)
     max = Math.floor(max)
 
-    return Math.floor(Math.random() * (max - min)) + min
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
