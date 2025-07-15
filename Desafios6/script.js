5e6efac01698da37291998fe51d9978a2e0848c4
Salkaiju <gabrielkarkotli@live.comm>
2025-07-15 10:56:14 -0300

    1

--- Desafios6/script.js
+++ Desafios6/script.js
@@ -27,11 +27,6 @@ switch(select){
 } break;
     case '2':{
         nome = readlineSync.question("\n Digite o nome: ");
-        if (nome && nome.trim() !== "") {
-            nomeParametro(nome);
-        } else {
-            console.log("\n Nome inválido.\n");
-        }
     } break;
     case '3':{
         numero = readlineSync.question("\n Digite o número: ");
@@ -45,7 +40,11 @@ switch(select){
         media(numero1, numero2, numero3);
     } break;
     case '5':{
-
+        console.log("\n Direi o máximo.");
+        numero1 = Number(readlineSync.question("\n Digite o número 1: "));
+        numero2 = Number(readlineSync.question("\n Digite o número 2: "));
+        numero3 = Number(readlineSync.question("\n Digite o número 3: "));
+        maior(numero1, numero2, numero3);
     } break;
     case '6':{
 
@@ -59,6 +58,7 @@ switch(select){
     } 
 } while (select != '0');
 
+// FUNÇÕES //
 
 function olaMundo(nome) {
     console.log("\n Olá, mundo! \n");
@@ -78,6 +78,13 @@ function media(numero1, numero2, numero3){
     let somaNumeros = (numero1 + numero2 + numero3);
     let mediaNumeros = (somaNumeros / 3);
     console.log(mediaNumeros.toFixed(2));
+}
+
+function maior(numero1, numero2, numero3){
+    let numeroMaior = numero1;
+    numeroMaior = numeroMaior < numero2 ? numeroMaior = numero2 : numeroMaior;
+    numeroMaior = numeroMaior < numero3 ? numeroMaior = numero3 : numeroMaior;
+    console.log("\n O maior número digitado foi: " + numeroMaior + "\n");
 }
 
 
