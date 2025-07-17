fbd7c11a51aa38f22d20b7c0d98315d4cc595521
Salkaiju <gabrielkarkotli@live.comm>
2025-07-17 10:27:03 -0300

    1

---
+++ Logica de programação_explore_funções_e_listas/Desafios4/script.js
@@ -0,0 +1,16 @@
+let listaGenerica = [];
+
+let linguagens = ["Javascript", "C", "C++", "Kotlin", "Python"];
+
+linguagens.push = ["Java", "Ruby", "GoLang"];
+
+let nome = ["Gabirl", "igirid", "marinan0"];
+let o = nome.length - 1;
+for(let i = 0; i <= o; i++){
+console.log(nome[i]);
+}
+
+
+
+
+
