aea6458d55d65b8cc6e6fa9bbb5863bcd30bb172
Gabriel <116319547+Salkaiju@users.noreply.github.com>
2025-07-23 11:38:09 -0300

    Delete Java_criando_a_asua_primeira_aplicacao directory

--- Java_criando_a_asua_primeira_aplicacao/main/src/principal/Estudos.java
+++
@@ -1,9 +0,0 @@
-package principal;
-
-public class Estudos {
-    public static void main(String[] args) {
-        String anotacao1 = "Java é horrível";
-        String anotacao2 = "A vida é triste";
-        System.out.println("\nAnotação 1: " + anotacao1 + "\n" + "Anotação 2: " + anotacao2 + "\n");
-    }
-}
